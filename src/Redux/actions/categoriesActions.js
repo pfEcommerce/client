47075e97503c8f034b176aad07b53cc82144f1b5
Rafaela <rafaelaru22@hotmail.com>
2021-11-12 02:27:48 -0300

    actions/reducer

---
+++ src/Redux/actions/categoriesActions.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+export const GETCATEGORIES = "GET_CATEGORIES";
+
+
+//funcion para traer todos las categorias
+export  function getCategories (){
+    return async function(dispatch) {
+    var res = await axios.get("http://localhost:3001/categories")
+    
+     dispatch({
+    type: GETCATEGORIES,
+    payload:res.data
+    });
+    };
+    };
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,41 @@
+import  { GETPRODUCTS } from  "./actions/productsActions.js" 
+import  { GETCATEGORIES } from  "./actions/categoriesActions.js" 
+
+
+
 const initialState = {
     games: [],
+    categories:[]
 }
 
 
 export default function reducer(state = initialState, action){
     switch(action.type){
+          case GETPRODUCTS:
+              return {
+              ...state,
+              games: action.payload,
+              }
+          case GETCATEGORIES:
+              return {
+              ...state,
+              categories:action.payload,
+              }
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         default: return state
     }
 }
