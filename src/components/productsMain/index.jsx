cfc742b1aa043a43fea6f4285c05e4ed7f4d673c
Santiago Iván Varacca <santiagovaracca@gmail.com>
2021-11-13 01:12:27 -0300

    animations

--- src/components/productsMain/index.jsx
+++ src/components/productsMain/index.jsx
@@ -1,19 +1,29 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import  ProductCard  from "../productCard"
 
+import ProductCard from "../productCard"
+import {useTransition, animated} from 'react-spring'
+import StyledButton from "../styles/styled_button/styledButton"
+import { useState } from "react"
 
-export default function ProductsMain ({mock}){
+export default function ProductsMain({ mock }) {
 
 
     // display: flex;
     // flex-wrap: wrap;
     // justify-content: space-evenly;
-    
+    const [estado,setEstado] = useState(true)
 
-    return(
-        <div style={{display: 'flex', 'flex-wrap':'wrap', 'justify-content':'space-evenly' , width : '80%' , margin: 'auto'}}>
-            {mock.length > 0 ? mock.map(e=><ProductCard key={e.id} p={e}/>) : 'Not found'}
+    const transition = useTransition(estado,{
+        from:{opacity:0 , y:'-200px'},
+        enter:{opacity:1 , y:'0px'},
+        leave:{opacity:0 , y:'-200px'},
+        delay:400
+    })
+
+
+    return (
+        <div style={{ display: 'flex', 'flex-wrap': 'wrap', 'justify-content': 'space-evenly', width: '80%', margin: 'auto' }}>
+                {/* <StyledButton onClick={()=>setEstado(!estado)}>ASD</StyledButton> */}
+                {transition((style,item)=> item ? mock.length > 0 ? mock.map(e => <animated.div style={style}><ProductCard key={e.id} p={e} /></animated.div>) : 'Not found' : '')}
         </div>
     )
 }
