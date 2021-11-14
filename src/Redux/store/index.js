41c171a32c0d327dd72d7c67224c22582f9cec58
leblurz <86071462+leblurz@users.noreply.github.com>
2021-11-14 19:36:26 -0300

    update logica products

--- src/Redux/store/index.js
+++ src/Redux/store/index.js
@@ -1,11 +1,25 @@
 import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import reducer from '../reducers';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
 
+function saveToLocalStorage(state) {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+}
+
+function loadFromLocalStorage() {
+//localStorage.clear()
+const serializedState = localStorage.getItem('state');
+if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+}
+
+const presistedState = loadFromLocalStorage();
 const store = createStore(
     reducer,
+    presistedState,
     composeWithDevTools(applyMiddleware(thunk)),
 );
-
+store.subscribe(() => saveToLocalStorage(store.getState()));
 export default store;
