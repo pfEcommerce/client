f83c3795a8ad463fcd526e66f8d7655d6a9f955e
leblurz <86071462+leblurz@users.noreply.github.com>
2021-11-14 23:53:05 -0300

    dislogg

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,7 +5,8 @@ import { useState, useEffect } from 'react';
 import { getProducts } from "../Redux/actions/productsActions.js";
 import { useDispatch , useSelector} from 'react-redux';
 import ProductsMain from './productsMain/index.jsx';
-import logger from '../Redux/actions/utilityActions.js';
+import {logger} from '../Redux/actions/utilityActions.js';
+import {dislogg} from '../Redux/actions/utilityActions.js';
 import { useAuth0 } from '@auth0/auth0-react'
 import ParticlesBackground from "../particlesBackground.jsx";
 import Footer from "./Footer/index.jsx";
@@ -22,6 +23,9 @@ export default function Prueba() {
 useEffect(() => {
   if (isAuthenticated) {
     dispatch(logger(user))
+  }
+  if (!isAuthenticated) {
+    dispatch(dislogg())
   }
   if(mockGames.length === 0) {
     dispatch(getProducts())
