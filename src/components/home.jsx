32a08ac3ecf6d1ec74ec336de126faceeff1cc6a
Martin Federico Pissoni <mpissoni24@gmail.com>
2021-11-15 11:41:27 -0300

    commit update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,14 @@
 import Login from "./Login/index.jsx"
 import Navbar from './navbar/index.jsx';
 import EmblaCarousel from './carousel/carousel.jsx';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { getProducts } from "../Redux/actions/productsActions.js";
 import { useDispatch , useSelector} from 'react-redux';
 import ProductsMain from './productsMain/index.jsx';
 import {logger} from '../Redux/actions/utilityActions.js';
 import {dislogg} from '../Redux/actions/utilityActions.js';
 import { useAuth0 } from '@auth0/auth0-react'
-import ParticlesBackground from "../particlesBackground.jsx";
-import Footer from "./Footer/index.jsx";
+
 
 export default function Prueba({game,setGame,setModalLogin,price,setPrice}) {
 
