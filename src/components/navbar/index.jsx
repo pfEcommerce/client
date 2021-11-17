b7d1a8e8076607ac37c22c26b406b2b4793a5b68
leblurz <86071462+leblurz@users.noreply.github.com>
2021-11-17 17:25:42 -0300

    correcciones

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -93,12 +93,12 @@ export default function Navbar({ game, setGame, price, setPrice }) {
         <StyledSearchbar placeholder="Search" />
       </div>
       <div className="icons">
+          <Link to="/" className="link">
         <div>
           <HomeIcon className="icon" />
-          <Link to="/" className="link">
             Home
-          </Link>
         </div>
+          </Link>
         <div onClick={showUserPanel}>
           {isAuthenticated ?
             <StyledUserImage>
