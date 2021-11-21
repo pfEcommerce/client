e006bc3472278f781b88a04d46d28290f94aabd9
Santiago Iván Varacca <santiagovaracca@gmail.com>
2021-11-21 15:32:08 -0300

    Update Login.jsx

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import StyledButton from "../styles/styled_button/styledButton";
 import {FiLogIn as LoginIcon} from "react-icons/fi"
 import dotenv from 'dotenv';
@@ -8,6 +9,7 @@ const h = process.env.AUTH0_BASE_URL || "http://localhost:3001";
 
 const Login = () => {
 
+  const { loginWithRedirect } = useAuth0();
   return <StyledButton onClick={() => loginWithRedirect({redirect_uri:h})}><LoginIcon/> Log In</StyledButton>;
 
 };
