69552d6a1e171f05215886e237eba5bd7d7c0077
Santiago Iván Varacca <santiagovaracca@gmail.com>
2021-11-22 12:31:08 -0300

    1

--- src/components/styles/styled_genres/styledGenres.js
+++ src/components/styles/styled_genres/styledGenres.js
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 import theme from "../theme"
 
-const StyledGenres = styled.div`
+const StyledGenres = styled.div `
     display: flex;
     flex-direction: row-reverse;
     color: ${theme.colors.mainGreen};
 
     & .genres {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: auto;
         position: relative;
         top: 6em;
         height: 53em; 
         margin-left: 1em;
 
-        & p {
+        /* & p {
             margin: 0;
         }
 
@@ -23,11 +23,20 @@ const StyledGenres = styled.div`
             background-color: ${theme.colors.mainGreen};
             color: ${theme.colors.mainGreen};
             margin: 0.5em 0;
-        }
+        } */
     }
 
-
+    .genOp{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #00000070;
+        margin: 0.125em;
+        padding: 0.5em;
+        cursor: pointer;
+        user-select: none;
+        border-radius: 0.5em;
+    }
 `
 
 export default StyledGenres
-
