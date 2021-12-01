04dda71d54995d60fcce2cd734bdadef8e5349d4
Martin Federico Pissoni <mpissoni24@gmail.com>
2021-12-01 13:34:08 -0300

    Edit Product

--- src/components/AdminPanel/QueryProducts/index.jsx
+++ src/components/AdminPanel/QueryProducts/index.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { editProduct, addCategoryToProduct, removeCategoryToProduct } from '../../../Redux/actions/productsActions';
-import { addCategory } from "../../../Redux/actions/categoriesActions"
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  editProduct,
+  addCategoryToProduct,
+  removeCategoryToProduct,
+} from "../../../Redux/actions/productsActions";
+import { addCategory } from "../../../Redux/actions/categoriesActions";
+import { StyledEditProduct } from "../../styles/styled_edit_product/styledEditProduct";
 
 function QueryProducts(props) {
-
   const dispatch = useDispatch();
 
-  const products = useSelector(state => state.rootReducer.games);
-  const dataCategories = useSelector(state => state.rootReducer.categories);
+  const products = useSelector((state) => state.rootReducer.games);
+  const dataCategories = useSelector((state) => state.rootReducer.categories);
 
   //Prductos Hooks
   // const [data, setData] = useState(productos);
@@ -26,13 +29,13 @@ function QueryProducts(props) {
   const [error, setError] = useState({});
 
   const [productoSeleccionado, setProductoSeleccionado] = useState({
-    id: '',
-    name: '',
-    price: '',
-    stock: '',
-    description: '',
-    images: '',
-    categories: []
+    id: "",
+    name: "",
+    price: "",
+    stock: "",
+    description: "",
+    images: "",
+    categories: [],
   });
 
   //Categorias Hooks
@@ -42,28 +45,31 @@ function QueryProducts(props) {
   const [modalInsertarCategoria, setModalInsertarCategoria] = useState(false);
 
   const [categoriaSeleccionada, setCategoriaSeleccionada] = useState({
-    id: '',
-    name: ''
+    id: "",
+    name: "",
   });
 
   //Productos
   const seleccionarProducto = (elemento, caso) => {
     setProductoSeleccionado(elemento);
-    (caso === 'Editar') ? setModalEditar(true) : setModalEliminar(true);
+    caso === "Editar" ? setModalEditar(true) : setModalEliminar(true);
   };
   //Categorias
   const seleccionarCategoria = (elemento, caso) => {
     setCategoriaSeleccionada(elemento);
-    (caso === 'Editar') ? setModalEditarCategoria(true) : setModalEliminarCategoria(true);
+    caso === "Editar"
+      ? setModalEditarCategoria(true)
+      : setModalEliminarCategoria(true);
   };
 
   //Productos
-  const handleChange = e => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name) setProductoSeleccionado((prevState) => ({
-      ...prevState,
-      [name]: value
-    }))
+    if (name)
+      setProductoSeleccionado((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
     // if (name === "image") {
     //   value = JSON.parse(localStorage.getItem('url'))
     //   setProductoSeleccionado({
@@ -77,10 +83,12 @@ function QueryProducts(props) {
     //     ...prevState,
     //     [name]: value
     //   }));
-    setError(validate({
-      ...productoSeleccionado,
-      [e.target.name]: [e.target.value]
-    }));
+    setError(
+      validate({
+        ...productoSeleccionado,
+        [e.target.name]: [e.target.value],
+      })
+    );
   };
 
   const validate = (input) => {
@@ -91,26 +99,28 @@ function QueryProducts(props) {
     // }
 
     if (!Number(input.price)) {
-      error.price = 'Please, input a valid price.';
-    } else if (String(input.price).charAt(String(input.price).length - 3) !== '.') {
-      error.price = 'Price format must have 2 digits after the decimal point.';
+      error.price = "Please, input a valid price.";
+    } else if (
+      String(input.price).charAt(String(input.price).length - 3) !== "."
+    ) {
+      error.price = "Price format must have 2 digits after the decimal point.";
     }
 
     if (Number(input.stock) < 0) {
-      error.stock = 'Please, input a valid amount.';
+      error.stock = "Please, input a valid amount.";
     }
 
     return error;
   };
 
   //Categorias
-  const handleChangeCategory = e => {
+  const handleChangeCategory = (e) => {
     const { name, value } = e.target;
     if (!value) return;
 
     setCategoriaSeleccionada((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -172,23 +182,38 @@ function QueryProducts(props) {
   const setCategoryToProduct = () => {
     if (!categoriaSeleccionada) return;
 
-    dispatch(addCategoryToProduct(productoSeleccionado.id, categoriaSeleccionada.categoryId));
+    dispatch(
+      addCategoryToProduct(
+        productoSeleccionado.id,
+        categoriaSeleccionada.categoryId
+      )
+    );
     setAddCategoryModal(false);
   };
 
   const unsetCategoryToProduct = () => {
     if (!categoriaSeleccionada) return;
 
-    dispatch(removeCategoryToProduct(productoSeleccionado.id, categoriaSeleccionada.categoryId));
+    dispatch(
+      removeCategoryToProduct(
+        productoSeleccionado.id,
+        categoriaSeleccionada.categoryId
+      )
+    );
     setRemoveCategoryModal(false);
   };
 
   return (
     <div className="App">
-      <h2 id='prodList' class="alert alert-info">Products List</h2>
+      <h2
+        style={{ margin: "auto", display: "flex", justifyContent: "center" }}
+        id="prodList"
+        class="alert alert-info"
+      >
+        Products List
+      </h2>
 
-      <br /><br />
-      <table className="table table-bordered" background-color="white">
+      {/* <table className="table table-bordered" background-color="white">
         <thead>
           <tr>
             <th>ID</th>
@@ -227,10 +252,43 @@ function QueryProducts(props) {
           ))
           }
         </tbody>
-      </table>
+      </table> */}
 
-      {
-        addCategory && <Modal isOpen={addCategoryModal}>
+      <StyledEditProduct>
+        {products &&
+          products.map((e) => (
+            <div className="content_cards">
+              <p>{e.name}</p>
+              <img src={e.image} alt="Hola mundo" />
+              <div className="buttons">
+                <div className="blue">
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => seleccionarProducto(e, "Editar")}
+                  >
+                    Edit Product
+                  </button>{" "}
+                  {"   "}
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => addCategoryToProductModal(e)}
+                  >
+                    Add Category
+                  </button>
+                </div>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeCategoryToProductModal(e)}
+                >
+                  Remove Category
+                </button>
+              </div>
+            </div>
+          ))}
+      </StyledEditProduct>
+
+      {addCategory && (
+        <Modal isOpen={addCategoryModal}>
           <ModalHeader>
             <div>
               <h3>Add category to product</h3>
@@ -238,73 +296,84 @@ function QueryProducts(props) {
           </ModalHeader>
           <ModalBody>
             <div className="form-group">
-
               <label>Category</label>
               <select
                 className="custom-select"
                 name="categoryId"
-                onChange={handleChangeCategory}>
-                <option selected value="" name="category">Select a category</option>
-                {dataCategories.map((e, i) =>
-                  <option value={e.id} name="category" key={i}>{e.name}</option>
-                )}
+                onChange={handleChangeCategory}
+              >
+                <option selected value="" name="category">
+                  Select a category
+                </option>
+                {dataCategories.map((e, i) => (
+                  <option value={e.id} name="category" key={i}>
+                    {e.name}
+                  </option>
+                ))}
               </select>
-
             </div>
-
           </ModalBody>
 
           <ModalFooter>
-            <button className="btn btn-primary"
-              onClick={() => setCategoryToProduct()}>
+            <button
+              className="btn btn-primary"
+              onClick={() => setCategoryToProduct()}
+            >
               Add
             </button>
             <button
               className="btn btn-danger"
-              onClick={() => setAddCategoryModal(false)}>
+              onClick={() => setAddCategoryModal(false)}
+            >
               Close
             </button>
           </ModalFooter>
         </Modal>
+      )}
 
-      }
+      {removeCategoryModal && (
+        <Modal isOpen={removeCategoryModal}>
+          <ModalHeader>
+            <div>
+              <h3>Remove category from product</h3>
+            </div>
+          </ModalHeader>
+          <ModalBody>
+            <div className="form-group">
+              <label>Category</label>
+              <select
+                className="custom-select"
+                name="categoryId"
+                onChange={handleChangeCategory}
+              >
+                <option selected value="" name="category">
+                  Select a category
+                </option>
+                {productoSeleccionado.categories.map((e) => (
+                  <option value={e.id} name="category">
+                    {e.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </ModalBody>
 
-      {removeCategoryModal && <Modal isOpen={removeCategoryModal}>
-        <ModalHeader>
-          <div>
-            <h3>Remove category from product</h3>
-          </div>
-        </ModalHeader>
-        <ModalBody>
-          <div className="form-group">
-
-            <label>Category</label>
-            <select
-              className="custom-select"
-              name="categoryId"
-              onChange={handleChangeCategory}>
-              <option selected value="" name="category">Select a category</option>
-              {productoSeleccionado.categories.map((e) =>
-                <option value={e.id} name="category">{e.name}</option>
-              )}
-            </select>
-
-          </div>
-
-        </ModalBody>
-
-        <ModalFooter>
-          <button className="btn btn-primary"
-            onClick={() => unsetCategoryToProduct()}>
-            Remove
-          </button>
-          <button
-            className="btn btn-danger"
-            onClick={() => setRemoveCategoryModal(false)}>
-            Close
-          </button>
-        </ModalFooter>
-      </Modal>}
+          <ModalFooter>
+            <button
+              className="btn btn-primary"
+              onClick={() => unsetCategoryToProduct()}
+            >
+              Remove
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => setRemoveCategoryModal(false)}
+            >
+              Close
+            </button>
+          </ModalFooter>
+        </Modal>
+      )}
 
       <Modal isOpen={modalEditar}>
         <ModalHeader>
@@ -321,7 +390,8 @@ function QueryProducts(props) {
               id="idNum"
               type="text"
               name="id"
-              value={productoSeleccionado && productoSeleccionado.id} />
+              value={productoSeleccionado && productoSeleccionado.id}
+            />
             <br />
 
             <label>Product</label>
@@ -331,7 +401,8 @@ function QueryProducts(props) {
               name="name"
               value={productoSeleccionado && productoSeleccionado.name}
               onChange={handleChange}
-              maxLength="200" />
+              maxLength="200"
+            />
             <br />
             <label>Price</label>
             <input
@@ -340,8 +411,9 @@ function QueryProducts(props) {
               name="price"
               value={productoSeleccionado && productoSeleccionado.price}
               onChange={handleChange}
-              maxLength="8" />
-            {error.price && <span className='text-danger'>{error.price}</span>}
+              maxLength="8"
+            />
+            {error.price && <span className="text-danger">{error.price}</span>}
             <br />
 
             <label>Stock</label>
@@ -354,8 +426,9 @@ function QueryProducts(props) {
               step="1"
               min="0"
               max="9999"
-              required />
-            {error.stock && <span className='text-danger'>{error.stock}</span>}
+              required
+            />
+            {error.stock && <span className="text-danger">{error.stock}</span>}
             <br />
 
             <label>Description</label>
@@ -365,7 +438,8 @@ function QueryProducts(props) {
               name="description"
               value={productoSeleccionado && productoSeleccionado.description}
               onChange={handleChange}
-              maxLength="2000" />
+              maxLength="2000"
+            />
             <br />
 
             <label>Image</label>
@@ -375,7 +449,8 @@ function QueryProducts(props) {
               name="image"
               value={productoSeleccionado && productoSeleccionado.image}
               onChange={handleChange}
-              maxLength="5000" />
+              maxLength="5000"
+            />
             <br />
             {/* <label>Image</label>
             <form></form>
@@ -384,17 +459,21 @@ function QueryProducts(props) {
               onChange={handleChange}
             />
             <br /> */}
-
           </div>
         </ModalBody>
 
         <ModalFooter>
-          <button disabled={error.price || error.stock} className="btn btn-primary" onClick={() => editar()}>
+          <button
+            disabled={error.price || error.stock}
+            className="btn btn-primary"
+            onClick={() => editar()}
+          >
             Update
           </button>
           <button
             className="btn btn-danger"
-            onClick={() => setModalEditar(false)}>
+            onClick={() => setModalEditar(false)}
+          >
             Cancel
           </button>
         </ModalFooter>
@@ -424,8 +503,9 @@ function QueryProducts(props) {
               className="form-control"
               type="text"
               name="name"
-              value={categoriaSeleccionada ? categoriaSeleccionada.name : ''}
-              onChange={handleChangeCategory} />
+              value={categoriaSeleccionada ? categoriaSeleccionada.name : ""}
+              onChange={handleChangeCategory}
+            />
             <br />
 
             <label>Description</label>
@@ -433,11 +513,13 @@ function QueryProducts(props) {
               className="form-control"
               type="text"
               name="description"
-              value={categoriaSeleccionada ? categoriaSeleccionada.description : ''}
-              onChange={handleChangeCategory} />
+              value={
+                categoriaSeleccionada ? categoriaSeleccionada.description : ""
+              }
+              onChange={handleChangeCategory}
+            />
             <br />
           </div>
-
         </ModalBody>
         {/* 
         <ModalFooter>
@@ -452,10 +534,9 @@ function QueryProducts(props) {
           </button>
         </ModalFooter> */}
       </Modal>
-    </div >
+    </div>
   );
 }
-
-export default QueryProducts;
+export default QueryProducts
 
 
